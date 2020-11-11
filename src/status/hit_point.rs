79c296f8579f36fe7846b051eb402eb5955aef99
yk-amarly-20 <y.k.83600260@icloud.com>
2020-11-11 17:13:07 +0900

    add: calculate hitpoint

---
+++ src/status/hit_point.rs
@@ -0,0 +1,17 @@
+use crate::error_handler::check_base::check_base;
+use crate::error_handler::check_individual::check_individual;
+use crate::error_handler::check_effort::check_effort;
+use crate::error_handler::check_level::check_level;
+
+pub fn calc_hit_point(_base: u32, _individual: u32, _effort: u32, level: u32) -> Result<u32, &'static str> {
+
+    let base = check_base(_base)?;
+    let individual = check_individual(_individual)?;
+    let effort = check_effort(_effort)?;
+    let level = check_level(_level)?;
+
+    let _hit_point: u32 = (base * 2 + individual + effort / 4) * level / 100;
+    let hit_point: u32 = _hit_point + level + 10;
+
+    Ok(hit_point)
+}
