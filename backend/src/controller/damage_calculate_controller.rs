70cfd7872c949d59a36b22a6fea3590c8bfaf45a
yk-amarly-20 <y.k.83600260@icloud.com>
2021-10-02 12:01:44 +0900

    いったん避難

--- backend/src/controller/damage_calculate_controller.rs
+++ backend/src/controller/damage_calculate_controller.rs
@@ -3,19 +3,26 @@ use actix_web::{get, web, Responder};
 use models::battle::attacker::Attacker;
 use models::battle::defender::Defender;
 use models::other_status::types::Types;
+use models::skill::skill::Skill;
 use serde::Deserialize;
 
+/*
+// TODO: いろいろ整ったら引数を一部nullableに
 #[derive(Deserialize)]
 pub struct DamageCaluculationParams {
     attacker_name: String,
     attacker_level: usize,
     skill_name: String,
-    attack_effort: Option<usize>,
-    special_attack_effort: Option<usize>,
+    // attack_effort: Option<usize>,
+    attack_effort: usize,
+    // special_attack_effort: Option<usize>,
+    special_attack_effort: usize,
     defender_name: String,
     defender_level: usize,
-    defense_effort: Option<usize>,
-    special_defense_effort: Option<usize>,
+    // defense_effort: Option<usize>,
+    defense_effort: usize,
+    // special_defense_effort: Option<usize>,
+    special_defense_effort: usize,
     terrain: Option<String>,
     climate: Option<String>,
 }
@@ -23,4 +30,16 @@ pub struct DamageCaluculationParams {
 #[get("/caluculate/damage")]
 async fn caluculate_damage(query: web::Query<DamageCaluculationParams>) -> impl Responder {
     // とりあえず適当なやつ置いておく
+
+    let attacker = Attacker::new(
+        query.attacker_level,
+        Types::Normal,
+        Types::Flying,
+        Skill::new("test", 50, Types::Flying),
+        query.attack_effort,
+        query.special_attack_effort,
+        32,
+        30,
+    );
 }
+*/
