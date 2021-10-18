f14d4546eff0f2bafac40c2d806f1e90e90fa78b
yk-amarly-20 <y.k.83600260@icloud.com>
2021-10-18 22:52:13 +0900

    パッケージ

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,6 +1,11 @@
+#[macro_use]
+extern crate diesel;
+extern crate chrono;
+
 pub mod controller;
+pub mod db;
 pub mod domain;
 pub mod interactor;
-pub mod repository;
+pub mod schema;
 pub mod usecase;
 pub mod utils;
